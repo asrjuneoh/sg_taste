db9822fef12778a4a76c5343c1a9866cb708c927
melon1024 <ohjs@sogang.ac.kr>
2022-09-30 14:24:21 +0900

    edit typo

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        비주기적인 맛집탐방<br></br>theme from by <Link to="https://github.com/stackrole/gatsby-starter-foundation">gatsby-starter-foundation</Link>
+        비주기적인 맛집탐방<br></br>theme from <Link to="https://github.com/stackrole/gatsby-starter-foundation">gatsby-starter-foundation</Link>
       </p>
     </div>
   </footer>
